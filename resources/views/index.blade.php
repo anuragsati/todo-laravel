37aa0f522d007ad2cba781828e98bede681cc5e8
anuragsati <anuragsati7@gmail.com>
2022-04-10 20:42:24 +0530

    added background image

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,16 +9,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Todo List</title>
+    <style>
+        section{
+            background-image: url("/blob.svg");
+            background-repeat: no-repeat;
+            background-size : cover;
+            background-attach: fixed;
+        }
+
+        #card{
+            background-color : rgba(255, 255, 255, 0.8);
+            border-radius : 10px;
+        }
+    </style>
 </head>
 <body>
     <section class="min-vh-100" style="background-color: #eee;">
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col col-lg-9 col-xl-7">
-                    <div class="card rounded-5">
+                <div class="col col-lg-9 col-xl-7" >
+                    <div class="card" id="card">
                         <div class="card-body p-4">
-                            <h4 class="text-center my-3 pb-3">To-Do List</h4>
-                            
+                            <h2 class="text-center my-3 pb-3">To-Do List</h2>
+
                             <form action="save_new_list" class="row row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2">
                                 <div class="col-12">
                                     <div class="form-outline">
